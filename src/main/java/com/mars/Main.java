33fe10a94370fe807a6f23a73a4ce08758078066
geyan <geyan0117@gmail.com>
2025-02-13 21:49:15 +0800

    init nettyproject

---
+++ src/main/java/com/mars/Main.java
@@ -0,0 +1,10 @@
+package com.mars;
+
+/**
+ * Created by geyan on 2025/2/13 21:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/com/mars/nio/NIOServer.java
@@ -0,0 +1,11 @@
+package com.mars.nio;
+
+/**
+ * Created by geyan on 2025/2/13 21:47
+ */
+public class NIOServer {
+
+    public static void main(String[] args) {
+
+    }
+}
